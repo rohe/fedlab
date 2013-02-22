eec7943a2b44f683219ba35f7be25dec5348ce7c
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2013-02-22 10:08:42 +0100

    Adding some test entities in config

--- frontend/etc/config.results.js
+++ frontend/etc/config.results.js
@@ -3,5 +3,7 @@
 	"938362": {"id": "ryo", "short": "Ryo", "name": "Ryos implementation"},
 	"287344": {"id": "openid4us", "short": "oic4us", "name": "Connect.openid4.us"},
 	"172534": {"id": "oicinfo", "short": "oic.info", "name": "Openidconnect.info"},
-	"273625": {"id": "connectopheroku", "short": "heroku", "name": "connect-op.heroku.com"}	
+	"273625": {"id": "connectopheroku", "short": "heroku", "name": "connect-op.heroku.com"},
+	"881622": {"id": "oxauth", "short": "gluu", "name": "Gluu oxAuth OpenID Connect 1.0 Implementation"},
+	"281726": {"id": "organge", "short": "orange", "name": "Orange.com"}
 }
